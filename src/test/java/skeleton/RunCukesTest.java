fe565cfa460a3e7b6fe7fd2d4f32e125f548922f
Mel <balim@users.noreply.github.com>
2018-07-22 16:21:12 -0400

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,7 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"})
+//@CucumberOptions(plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"})
+@Cucumber.Options(format = {"pretty", "html:target/Destination"} )
 //@CucumberOptions(tags = "@ok", plugin = { "skeleton.RpListener" })
 public class RunCukesTest {
 }
