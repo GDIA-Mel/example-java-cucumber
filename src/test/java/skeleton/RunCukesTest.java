421b8faa417920a7f2f2878067651fff69cf36a4
Mel <balim@users.noreply.github.com>
2018-07-22 16:22:27 -0400

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -3,6 +3,7 @@ package skeleton;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 //@CucumberOptions(plugin = {"pretty", "com.epam.reportportal.cucumber.ScenarioReporter"})
